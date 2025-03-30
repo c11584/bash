70bbccb91647032efb6eae3ee38488c3a9bf1f05
c11584 <c11584@163.com>
2025-03-31 00:57:43 +0800

    Update limit-port.sh

--- limit-port.sh
+++ limit-port.sh
@@ -4,8 +4,8 @@ interfaces=$(ip link show | grep -oP '^\d+: \K\w+' | grep -v '^tun' | grep -v 'l
 port=$1
 limit=$2
 
-tc qdisc add dev eth0 root handle 1: htb
-tc class add dev eth0 parent 1: classid 1:$limit htb rate ${limit}Mbit ceil ${limit}Mbit
-tc filter add dev eth0 parent 1: protocol ip u32 match ip sport $port 0xffff classid 1:$limit
+tc qdisc add dev $interfaces root handle 1: htb
+tc class add dev $interfaces parent 1: classid 1:$limit htb rate ${limit}Mbit ceil ${limit}Mbit
+tc filter add dev $interfaces parent 1: protocol ip u32 match ip sport $port 0xffff classid 1:$limit
 
 echo -e "limit success"
