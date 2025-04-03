df2d8d7bb96bd714662ce9e948023d32bdb8a547
c11584 <c11584@163.com>
2025-04-03 15:16:10 +0800

    Update exclude-google.sh

--- exclude-google.sh
+++ exclude-google.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+dig pay.google.com +short >google_ipv4.txt
+dig +short pay.google.com AAAA >google_ipv6.txt
+while read line; do ipset add google_pay_block_v4 $line; done <google_ipv4.txt
+while read line; do ipset add google_pay_block_v6 $line; done <google_ipv6.txt
 
-dig pay.google.com  +short  > google_ipv4.txt
-while read line; do ipset add google_pay_block_v4 $line; done < google_ipv4.txt  
+dig payments.google.com +short >google_ipv4.txt
+dig +short payments.google.com AAAA >google_ipv6.txt
+while read line; do ipset add google_pay_block_v4 $line; done <google_ipv4.txt
+while read line; do ipset add google_pay_block_v6 $line; done <google_ipv6.txt
 
-dig payments.google.com  +short  > google_ipv4.txt
-while read line; do ipset add google_pay_block_v4 $line; done < google_ipv4.txt  
+dig wallet.google.com +short >google_ipv4.txt
+dig +short wallet.google.com AAAA >google_ipv6.txt
+while read line; do ipset add google_pay_block_v4 $line; done <google_ipv4.txt
+while read line; do ipset add google_pay_block_v6 $line; done <google_ipv6.txt
 
-dig wallet.google.com  +short  > google_ipv4.txt
-while read line; do ipset add google_pay_block_v4 $line; done < google_ipv4.txt  
+dig play-fe.googleapis.com +short >google_ipv4.txt
+dig +short play-fe.googleapis.com AAAA >google_ipv6.txt
+while read line; do ipset add google_pay_block_v4 $line; done <google_ipv4.txt
+while read line; do ipset add google_pay_block_v6 $line; done <google_ipv6.txt
 
-dig play-fe.googleapis.com +short  > google_ipv4.txt
-while read line; do ipset add google_pay_block_v4 $line; done < google_ipv4.txt  
-
-
-
-iptables -I INPUT -m set --match-set google_pay_block_v4 src -j DROP 
+iptables -I INPUT -m set --match-set google_pay_block_v4 src -j DROP
+iptables -I INPUT -m set --match-set google_pay_block_v6 src -j DROP
