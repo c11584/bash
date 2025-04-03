3807c1fbcb83669b928b940b7729d2afdce542c7
c11584 <c11584@163.com>
2025-04-03 16:11:34 +0800

    Update run-exclude-google.sh

--- run-exclude-google.sh
+++ run-exclude-google.sh
@@ -2,7 +2,7 @@
 yum install squid ipset bind-utils -y
 
 ipset create google_pay_block_v4 hash:net family inet
-ipset create google_pay_block_v6 hash:net family inet
+ipset create google_pay_block_v6 hash:net family inet6
 iptables -A OUTPUT -d 8.8.8.8 -p tcp --dport 853 -j DROP
 
 cd /usr/local/
@@ -11,5 +11,7 @@ chmod +x /usr/local/time.sh
 
 sh -c 'echo "* * * * * root /usr/local/time.sh  >>/var/log/script.log  2>&1" >> /etc/crontab'
 
+iptables -I INPUT -m set --match-set google_pay_block_v4 src -j DROP
+ip6tables -A OUTPUT -m set --match-set google_pay_block_v6 dst -j DROP 
+
 systemctl restart crond
-echo -e "install success"
