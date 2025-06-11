d41111f199fc00f47016ff4c9979f79e2c3c9abd
c11584 <c11584@163.com>
2025-06-11 14:19:30 +0800

    Create l2tp.sh

---
+++ l2tp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo su root 
+yum -y install wget
+wget https://get.vpnsetup.net -O vpn.sh
+sudo VPN_IPSEC_PSK='3' VPN_USER='3' VPN_PASSWORD='3'
+
+
+systemctl stop firewalld
+systemctl mask firewalld
+apt install -y iptables
+apt install -y iptables-services
+iptables -P INPUT ACCEPT
+iptables -F
+iptables -F
+iptables -X
+iptables -Z
+service iptables save
+systemctl restart iptables
+systemctl status iptables
+systemctl status ipsec
+systemctl status xl2tpd
+systemctl enable ipsec
+systemctl enable xl2tpd
+systemctl restart iptables
+systemctl restart ipsec
+systemctl restart xl2tpd
+systemctl start iptables
+systemctl start ipsec
+systemctl start xl2tpd
+
+echo -e "install success"
