ade86c4df8b1e9e29dbdaf01b9b2e367853655df
c11584 <c11584@163.com>
2026-05-15 20:42:02 +0800

    Update repassword.sh

--- repassword.sh
+++ repassword.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
-
+    
+PASSWORD=${1:-Aa6190360@}
 cat /dev/null > /root/.ssh/authorized_keys
-echo root:Aa6190360@| chpasswd root
+echo root:${PASSWORD}| chpasswd root
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
-
 
 echo -e "install success"
